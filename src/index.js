33c89cb6e890b1a9b1620cc068725614e92fd874
savanikrishna95 <savanitina@outlook.com>
2024-01-22 14:07:12 +0530

    make changes

--- src/index.js
+++ src/index.js
@@ -6,15 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(registration => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(error => {
-      console.error('Service Worker registration failed:', error);
-    });
-}
+
 
 root.render(
   // <React.StrictMode>
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,42 +1,5 @@
-// service-worker.js
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('my-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          // Add other paths to your assets
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-
-
-  // service-worker.js
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== 'my-cache') {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      return self.clients.claim();
-    })
-  );
-});
-
+setTimeout(() => 
+    window.location.reload(true)
+, 5000); 
   
