2119c903cd20412026b4b7c229844da54cfd0d02
savanikrishna95 <savanitina@outlook.com>
2024-01-22 13:56:42 +0530

    make changes

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-this.addEventListener('install', event => {
+self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('my-cache').then(cache => {
         return cache.addAll([
@@ -12,7 +12,7 @@ this.addEventListener('install', event => {
     );
   });
   
-  this.addEventListener('fetch', event => {
+  self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(response => {
         return response || fetch(event.request);
@@ -23,20 +23,20 @@ this.addEventListener('install', event => {
 
   // service-worker.js
 
-this.addEventListener('activate', event => {
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheName !== 'my-cache') {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      }).then(() => {
-        return this.clients.claim();
-      })
-    );
-  });
-  
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== 'my-cache') {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
   
